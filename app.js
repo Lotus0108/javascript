882f5bfba5c6a837248c7de24294ba22bea105a3
岡村蓮 <okamuraren@okamurarennoMacBook-Pro.local>
2022-03-08 19:12:03 +0900

    [Start]

---
+++ app.js
@@ -0,0 +1,25 @@
+let hello = 'Hello World';
+alert(hello);
+
+//整数を代入
+let int1 = 1
+
+//負数を代入
+let int2 = -10
+
+//少数を代入
+let float1 = 3.14
+
+//文字列を代入
+let str1 = 'javascriptを覚えよう'
+
+let max = 100;
+let num = 1;
+let count = 0;
+
+while(num < max){
+  num = num * 2;
+  count = count + 1;
+}
+
+alert('2を掛け続けて' + max + 'を超えるのに必要だった回数は' + count + '回です');
