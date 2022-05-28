231ad4d9866375005d8cb2abea6190252cfb2eb3
岡村蓮 <ren140108@icloud.com>
2022-05-28 21:34:58 +0900

    start

--- app.js
+++ app.js
@@ -13,6 +13,23 @@ let float1 = 3.14
 //文字列を代入
 let str1 = 'javascriptを覚えよう'
 
+alert('Hello' + 'World');
+
+let str2 = 'Hello';
+let str3 = 'World!!';
+alert(str2 + str3);
+
+let orange = 100;
+let apple = 120;
+
+if(orange < apple){
+  alert('みかんの値段がりんごより安い');
+} else if(orange == apple){
+  alert('みかんとりんごが同じ値段');
+} else{
+  alert('みかんの値段がりんごより高い');
+}
+
 let max = 100;
 let num = 1;
 let count = 0;
