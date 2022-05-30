85bb9a7ff63819900b6abbbae37af47b5721c1a5
岡村蓮 <ren140108@icloud.com>
2022-05-30 15:13:19 +0900

    while

--- app.js
+++ app.js
@@ -1,35 +1,3 @@
-let hello = 'Hello World';
-alert(hello);
-
-//整数を代入
-let int1 = 1
-
-//負数を代入
-let int2 = -10
-
-//少数を代入
-let float1 = 3.14
-
-//文字列を代入
-let str1 = 'javascriptを覚えよう'
-
-alert('Hello' + 'World');
-
-let str2 = 'Hello';
-let str3 = 'World!!';
-alert(str2 + str3);
-
-let orange = 100;
-let apple = 120;
-
-if(orange < apple){
-  alert('みかんの値段がりんごより安い');
-} else if(orange == apple){
-  alert('みかんとりんごが同じ値段');
-} else{
-  alert('みかんの値段がりんごより高い');
-}
-
 let max = 100;
 let num = 1;
 let count = 0;
